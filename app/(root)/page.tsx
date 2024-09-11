7a06b33259c9dd6d56134dcc560aa12ce1c24d02
karchaoui Abdessamad <140338304+abdok10@users.noreply.github.com>
2024-09-11 17:24:42 +0100

    store modal added and zustand setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,23 @@
-import { UserButton } from "@clerk/nextjs";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
 
 export default function SetupPage() {
+  const { isOpen, onOpen } = useStoreModal();
+  console.log(isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
   return (
-    <div>
-      <p>Dashboard</p>
-      <UserButton />
+    <div className="p-4">
+      <p>Root Page</p>
+      <Button onClick={() => {}}>Open Modal</Button>
     </div>
   );
 }
