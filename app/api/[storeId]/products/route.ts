4c4e5ebded1008cb4ed7bf378f632ad312e89054
karchaoui Abdessamad <140338304+abdok10@users.noreply.github.com>
2024-11-10 22:17:16 +0100

    Product Api routes done

--- app/api/[storeId]/products/route.ts
+++ app/api/[storeId]/products/route.ts
@@ -10,7 +10,16 @@ export const POST = async (
     const { userId } = auth();
     const body = await req.json();
 
-    const { name, value } = body;
+    const {
+      name,
+      price,
+      images,
+      categoryId,
+      sizeId,
+      colorId,
+      isFeatured,
+      isArchived,
+    } = body;
 
     if (!userId) {
       return new NextResponse("Unauthenticated", { status: 401 });
@@ -20,10 +29,26 @@ export const POST = async (
       return new NextResponse("Name is required", { status: 400 });
     }
 
-    if (!value) {
-      return new NextResponse("Value is required", { status: 400 });
+    if (!images || !images.length) {
+      return new NextResponse("Images are required", { status: 400 });
     }
-    
+
+    if (!price) {
+      return new NextResponse("Price is required", { status: 400 });
+    }
+
+    if (!categoryId) {
+      return new NextResponse("Category id is required", { status: 400 });
+    }
+
+    if (!sizeId) {
+      return new NextResponse("Size id is required", { status: 400 });
+    }
+
+    if (!colorId) {
+      return new NextResponse("Color id is required", { status: 400 });
+    }
+
     if (!params.storeId) {
       return new NextResponse("Store id is Required", { status: 400 });
     }
@@ -39,41 +64,69 @@ export const POST = async (
       return new NextResponse("Unauthorized", { status: 403 });
     }
 
-    const color = await db.color.create({
+    const product = await db.product.create({
       data: {
         name,
-        value,
+        price,
+        categoryId,
+        sizeId,
+        colorId,
+        isFeatured,
+        isArchived,
         storeId: params.storeId,
+        images: {
+          createMany: {
+            data: [...images.map((image: { url: string }) => image)],
+          },
+        },
       },
     });
 
-    return NextResponse.json(color, { status: 201 });
-  
-} catch (error) {
-    console.log('[COLORS_POST]', error)
+    return NextResponse.json(product, { status: 201 });
+  } catch (error) {
+    console.log("[PRODUCTS_POST]", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 };
 
 export const GET = async (
-    req: Request,
-    { params }: { params: { storeId: string } }
+  req: Request,
+  { params }: { params: { storeId: string } }
 ) => {
   try {
+    const { searchParams } = new URL(req.url);
+    const categoryId = searchParams.get("categoryId") || undefined;
+    const sizeId = searchParams.get("sizeId") || undefined;
+    const colorId = searchParams.get("colorId") || undefined;
+    const isFeatured = searchParams.get("isFeatured") || undefined;
 
     if (!params.storeId) {
-        return new NextResponse("Store id is Required", { status: 400 });
+      return new NextResponse("Store id is Required", { status: 400 });
     }
 
-    const colors = await db.color.findMany({
-        where: {
-            storeId: params.storeId,
-        }
+    const products = await db.product.findMany({
+      where: {
+        storeId: params.storeId,
+        categoryId,
+        sizeId,
+        colorId,
+        isFeatured: isFeatured ? true : undefined,
+        isArchived: false,
+      },
+      include: {
+        images: true,
+        category: true,
+        color: true,
+        size: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
     });
 
-    return NextResponse.json(colors, { status: 201 });
-} catch (error) {
-    console.log('[COLORS_GET]', error)
+    return NextResponse.json(products, { status: 201 });
+  } catch (error) {
+    console.log("[PRODUCTD_GET]", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 };
